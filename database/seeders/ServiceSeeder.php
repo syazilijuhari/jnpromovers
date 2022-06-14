cd6cca7e5b0331c481c93cb0140e2747639d01e7
syazilijuhari <74142207+syazilijuhari@users.noreply.github.com>
2022-06-15 00:24:39 +0800

    Update

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -24,13 +24,27 @@ class ServiceSeeder extends Seeder
 
         DB::table('services')->insert([
             'id' => 'S102',
+            'title' => 'Dismantle',
+            'category' => 'service',
+            'description' => 'Experienced manpower  will help to dismantle the furniture using the correct technique and tools.'
+        ]);
+
+        DB::table('services')->insert([
+            'id' => 'S103',
+            'title' => 'Wrapping',
+            'category' => 'service',
+            'description' => 'Wrapping your items using shrink and bubble wrap to protect from any damage during moving.'
+        ]);
+
+        DB::table('services')->insert([
+            'id' => 'S104',
             'title' => '1 Ton Lorry',
             'category' => 'transport',
             'description' => 'We have 1 Ton Lorry that could help you to load small quantity of items'
         ]);
 
         DB::table('services')->insert([
-            'id' => 'S103',
+            'id' => 'S105',
             'title' => '3 Ton Lorry',
             'category' => 'transport',
             'description' => 'We have 3 Ton Lorry that could help you to load large quantity of items'
--- resources/views/dashboards/customer/booking-two.blade.php
+++ resources/views/dashboards/customer/booking-two.blade.php
@@ -15,4 +15,18 @@
         </div>
     </div>
 
+    <div class="container">
+        <form class="row g-3">
+            <div class="col-md-2">
+                <input type="text" class="form-control" disabled style="background-color: white; border: black;">
+            </div>
+            <div class="col-auto">
+                <a href="{{route('customer.booking-one')}}" type="submit" class="btn btn-danger" style="color: white">Back <span><i class="fas fa-arrow-left"></i></span></a>
+            </div>
+            <div class="col-auto">
+                <a href="{{route('customer.booking-two')}}" type="submit" class="btn btn-danger" style="color: white">Next <span><i class="fas fa-arrow-right"></i></span></a>
+            </div>
+        </form>
+    </div>
+
 @endsection
