1bb517a8eb66fb377a7565806a6f10de5cb2bf79
syazilijuhari <74142207+syazilijuhari@users.noreply.github.com>
2022-07-01 11:55:24 +0800

    Update send email

--- resources/views/layouts/viewer.blade.php
+++ resources/views/layouts/viewer.blade.php
@@ -13,7 +13,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     {{--    <script src="{{ asset('js/map.js') }}" defer></script>--}}
-    <script src="{{ asset('js/email.js') }}" defer></script>
+{{--    <script src="{{ asset('js/email.js') }}" defer></script>--}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
